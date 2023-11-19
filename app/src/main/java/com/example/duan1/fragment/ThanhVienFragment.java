71904563c0a08c605f5eaf60cf7f69057ae11e0f
Longvu003 <130145722+Longvu003@users.noreply.github.com>
2023-11-19 20:30:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/duan1/fragment/ThanhVienFragment.java
@@ -0,0 +1,16 @@
+package com.example.duan1.fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.duan1.R;
+
+public class ThanhVienFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thanh_vien_fragment);
+    }
+}
---
+++ app/src/main/java/com/example/duan1/fragment/YeuThichFragment.java
@@ -0,0 +1,16 @@
+package com.example.duan1.fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.duan1.R;
+
+public class YeuThichFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yeu_thich_fragment);
+    }
+}
