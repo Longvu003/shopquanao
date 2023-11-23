ee52519305dc69214a062c0b3c2e9f58b1c1abd2
TienLeXuan <lexuantien977@gmail.com>
2023-11-23 19:18:29 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,6 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     implementation ("com.github.bumptech.glide:glide:4.16.0")
+
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 }
