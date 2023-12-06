68d29b59e55fc3a69dd5534214e81979ea42bad6
Longvu003 <130145722+Longvu003@users.noreply.github.com>
2023-12-06 18:41:25 +0700

    add them nha

--- app/src/main/java/com/example/duan1/GiohangActivity.java
+++ app/src/main/java/com/example/duan1/GiohangActivity.java
@@ -28,20 +28,7 @@ private RecyclerView recyclerView_giohang;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_giohang);
-//        recyclerView_giohang=findViewById(R.id.recycler_view_giohang);
-//        gioHang=getIntent().<SanPham>getParcelableArrayListExtra("gioHang");
-//        giohangAdapter=new SanPhamAdapter(this,gioHang);
-//        recyclerView_giohang.setLayoutManager(new LinearLayoutManager(this));
-//        recyclerView_giohang.setAdapter(giohangAdapter);
-//        Intent intent = getIntent();
-//        if (intent!=null){
-//            String masp = intent.getStringExtra("masp");
-//            String tensp = intent.getStringExtra("tensp");
-//            String giasp = intent.getStringExtra("giasp");
-//            String hinhanhsp = intent.getStringExtra("hinhanhsp");
-//            total+=giasp;
-//            Log.d("IntentData", "masp: " + masp + ", tensp: " + tensp + ", giasp: " + giasp + ", hinhanhsp: " + hinhanhsp);
-//        }
+
 
     }
 
