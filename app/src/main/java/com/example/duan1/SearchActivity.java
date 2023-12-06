25261a1eaa0731c4a16d1c5cdb92ff508357ea21
TienLeXuan <151633678+TienLeXuan@users.noreply.github.com>
2023-12-06 15:33:27 +0700

    add

--- app/src/main/java/com/example/duan1/SearchActivity.java
+++ app/src/main/java/com/example/duan1/SearchActivity.java
@@ -1,10 +1,16 @@
 package com.example.duan1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class SearchActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    EditText edtsearch;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
