f75632c5ed1ea7ccabe5ea65821c9e9a72963d02
TienLeXuan <151633678+TienLeXuan@users.noreply.github.com>
2023-12-12 20:51:35 +0700

    ....

--- app/src/main/java/com/example/duan1/Model/MySharedPreferences.java
+++ app/src/main/java/com/example/duan1/Model/MySharedPreferences.java
@@ -1,6 +1,7 @@
 package com.example.duan1.Model;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 
 public class MySharedPreferences {
@@ -22,4 +23,17 @@ public class MySharedPreferences {
         SharedPreferences sharedPreferences = mContext.getSharedPreferences(MY_SHARE_FREFERENCES, Context.MODE_PRIVATE);
         return sharedPreferences.getBoolean(key, false);
     }
+
+    public void putStringValue(String key, String value) {
+        SharedPreferences sharedPreferences = mContext.getSharedPreferences(MY_SHARE_FREFERENCES, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    public String getStringValue(String key) {
+        SharedPreferences sharedPreferences = mContext.getSharedPreferences(MY_SHARE_FREFERENCES, Context.MODE_PRIVATE);
+        return sharedPreferences.getString(key, "");
+    }
+
 }
